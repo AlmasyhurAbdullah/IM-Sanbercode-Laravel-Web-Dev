0920afccf1c6930df9aad79b7423e556edba06be
Abdullah <almasyhurabdullah@gmail.com>
2025-05-07 10:43:17 +0700

    menambahkan tugas 12

---
+++ reviewbook/resources/views/register.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buat form baru</title>
+</head>
+<body>
+    <h1>Buat Account Baru!</h1>
+    <h2>Sign Up Form</h2>
+
+    <form>
+        <label>First Name:</label><br><br>
+        <input type="text"><br><br>
+
+        <label>Last Name:</label><br><br>
+        <input type="text"><br><br>
+
+        <label>Gender</label><br><br>
+        <input type="radio" name="status">Male<br>
+        <input type="radio" name="status">Female<br>
+        <input type="radio" name="status">Other<br><br>
+
+        <label>Nationality</label><br><br>
+        <select name="Nationality">
+            <option value="">Indonesian</option>
+            <option value="">Malaysian</option>
+            <option value="">Singaporean</option>
+        </select><br><br>
+
+        <label>Language Spoken:</label><br><br>
+        <input type="checkbox" name="status">Indonesia<br>
+        <input type="checkbox" name="status">English<br>
+        <input type="checkbox" name="status">Other<br><br>
+
+        <label>Bio:</label><br><br>
+        <textarea rows="5"></textarea><br><br>
+
+        <a href="welcome">Sign Up</a>
+
+
+    </form>
+</body>
+</html>
